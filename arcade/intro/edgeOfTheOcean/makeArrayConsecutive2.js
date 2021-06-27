7c9b950662778d18755557134b97d8ac03e79087
Hien Nguyen <hnguy179@gmail.com>
2021-06-27 10:43:17 -0700

    Planned makeArrayConsecutive2

---
+++ arcade/intro/edgeOfTheOcean/makeArrayConsecutive2.js
@@ -0,0 +1,18 @@
+function makeArrayConsecutive2(statues) {
+  let sortedStatues = statues.sort()
+  let firstValue = sortedStatues[0]
+  for (let i = 1; i < sortedStatues.lengt; i++) {
+
+  }
+}
+
+//sort statues arr
+//loop through arr
+//[2,3,6,8]
+//statues[0] = 2
+//statues[i+1] === 3 ? yes, return
+//statues[i+1] === 4 ? no, counter+
+//statues[i+1] === 5 ? no, counter++
+//statues[i+1] === 6 ? yes return
+//statues[i+1] === 7 ? no, counter++
+//statues[i+1] === 8 ? yes, return
